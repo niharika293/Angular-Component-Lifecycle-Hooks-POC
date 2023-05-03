4a75ab195c41822a37f37a96dc0405179b88baf3
Niharika Gurnani <niharika.gurnani@solvezy.com>
2023-05-03 19:10:24 +0530

    Parent & child lifecycle methods altogether

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,49 @@
-import { Component, OnChanges, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnChanges {
+export class AppComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
 
   color : any = 'blue';
   constructor(){
-    // console.log("inside app component constructor, called before ngOnInit");
+    console.log("inside parent's constructor, called before ngOnInit");
   }
 
   ngOnInit() {
-    // console.log("Inside app- component ngOnInit");
+    console.log("Inside parent's ngOnInit");
   }
 
   ngOnChanges(){
-    // console.log("Inside app- component ngOnChanges");
+    console.log("Inside parent's ngOnChanges");
   }
 
   changeColor(color : any){
    this.color = color;
   }
 
-  title = 'angular-components-lifecyele-poc';
+  ngDoCheck(){
+    console.log("Inside parent's ngDoCheck");
+    // Why getting called multiple times. 
+  }
+
+  ngAfterContentInit() {
+    console.log("Inside parent's ngAfterContentInit");
+  }
+
+  ngAfterContentChecked() {
+    console.log("Inside parent's ngAfterContentChecked");
+  }
+
+  ngAfterViewInit() {
+    console.log('Inside parent"s AfterviewInit');
+  }
+
+  ngAfterViewChecked(){
+    console.log("Inside parent's ngAfterViewChecked");
+  }
+
+  
 }
